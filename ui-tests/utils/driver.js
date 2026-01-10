23bff86101fe7964cc046dc83d03a820ae7af94e
maliniravindranath2024 <malini.ravindranath@eleve.isep.fr>
2026-01-10 09:38:41 +0100

    Fix for ui-test errors

--- ui-tests/utils/driver.js
+++ ui-tests/utils/driver.js
@@ -1,8 +1,19 @@
 const { Builder } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 async function getDriver() {
+  const options = new chrome.Options()
+    .addArguments(
+      "--headless=new",
+      "--disable-gpu",
+      "--no-sandbox",
+      "--disable-dev-shm-usage"
+    )
+    .setChromeBinaryPath(process.env.CHROME_BIN || "/usr/bin/google-chrome");
+
   return await new Builder()
-    .forBrowser("MicrosoftEdge")
+    .forBrowser("chrome")
+    .setChromeOptions(options)
     .build();
 }
 
